03bfc14ed59ef6a1ae5378d7a5b2a81b041d4392
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-03-09 16:23:51 +0200

    init the databse sql file with users table

---
+++ db/db.sql
@@ -0,0 +1,18 @@
+/**
+ * Author:  mohamed
+ * Created: Mar 7, 2026
+ */
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id INT AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    profile_picture VARCHAR(255),
+    bio VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
