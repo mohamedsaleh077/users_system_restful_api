bb86a456f6775bf771ea75ddb95d15b418697788
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-03-09 17:17:10 +0200

    setting up Router class

---
+++ website/app/Core/Router.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Core;
+
+/**
+ * Description of Router
+ *  Decide what to do based on the Action and Params of the Route
+ *      Route / Action / Params
+ *  Route is the method that App Class is calling
+ * @author mohamed
+ */
+class Router {
+
+}
