36cfa3c13c85a5efb075ace01b899dce683e9b22
Mohammed Saleh <mohammed.saleh.22707@gmail.com>
2026-03-10 22:47:42 +0200

    Init JWT Controller with encoding support

---
+++ website/app/Core/JWT.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Core;
+
+/**
+ * Description of JWT
+ * jor generating and validating JWT tokens
+ *
+ * @author mohamed
+ */
+class JWT {
+    const KEY1 = "3f85ae090171d3c4d170fd65caaa13eed8b8d325bdb30a285f2fc945c5146a4f";
+    const KEY2 = "30944830c305a65676c67929d440d893a8401b6dedd35f626d6c961c3e604d39";
+    
+    private function Base64URLEncode(string $text): string
+    {
+        return str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($text));
+    }
+    
+    public function Encode(array $payload): string
+    {
+         $header = json_encode([
+            "alg" => "HS256",
+            "typ" => "JWT"
+        ]);
+
+        $header = $this->Base64URLEncode($header);
+        
+        $payload = $this->Base64URLEncode(json_encode($payload));
+        
+        $signature = hash_hmac("sha256", $header . "." . $payload, self::KEY1, true);
+        $signature = $this->Base64URLEncode($signature);
+        
+        return $header . "." . $payload . "." . $signature;
+    }
+    
+    
+}
